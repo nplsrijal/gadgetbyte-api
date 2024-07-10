da7088684d62e5075ba6e8f30f66953e2fdafc5a
Shreejal Nepal <shreejal.nepal@midastechnologies.com.np>
2024-07-10 16:19:13 +0545

    chagne

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,6 @@ Route::prefix('v1')->middleware(['apiMiddleware','verify_header'])->group(functi
     Route::apiResource('attributes', AttributeController::class);
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('vendors', VendorController::class);
-    Route::apiResource('attributes', AttributeController::class);
     Route::apiResource('attribute-options', AttributeOptionController::class);
     Route::apiResource('variations', VariationController::class);
     Route::apiResource('products', ProductController::class);
