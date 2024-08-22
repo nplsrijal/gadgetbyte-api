c488a7823a7188f9aa6bef7596d5fd4d14e4e613
Shreejal Nepal <shreejal.nepal@midastechnologies.com.np>
2024-08-22 08:40:34 +0545

    commentliked disliked add

--- app/Models/BlindCameraTest.php
+++ app/Models/BlindCameraTest.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class BlindCameraTest extends Model
+class BlindCameraTest extends Model implements Auditable
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
+    const DELETED_AT = 'archived_at';
+    protected $guarded=[];
 }
